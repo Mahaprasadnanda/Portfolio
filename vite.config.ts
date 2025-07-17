cdd0ce44eb14c66eb558f2fe1abb8003814e9e83
Mahaprasad Nanda <140647643+Mahaprasadnanda@users.noreply.github.com>
2025-07-18 00:47:22 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 this is the important fix for Vercel build
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
